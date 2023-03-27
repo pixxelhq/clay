38217fe92ad9bb06ea14c60f5e31535961528dcc
Akash Palrecha <akash@pixxel.co.in>
2023-03-27 19:15:28 +0530

    feat: added `create-dockerfile` command to `ramen` (#42)
    * feat: added create-dockerfile command
    
    * chore: updated go.mod & go.sum
    
    * fix: updated dockerfile test
    
    * fix: fixed function names & scope

--- cmd/create.go
+++ cmd/create.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -8,8 +7,11 @@ import (
 	"fmt"
 
 	"github.com/example/ramen/pkg/bootstrap"
+	"github.com/example/ramen/pkg/dockerfile"
 	"github.com/spf13/cobra"
 )
+
+var useHttpRunner bool
 
 // createCmd represents the create command
 var createCmd = &cobra.Command{
@@ -28,7 +30,27 @@ var createCmd = &cobra.Command{
 	},
 }
 
-func init() {
-	rootCmd.AddCommand(createCmd)
+var dockerCmd = &cobra.Command{
+	Use:   "create-dockerfile [modelSpecificationPath] [sourceCodeFolder] [useHttpRunner]",
+	Short: "Creates a dockerfile to package and serve your model",
+	Long: `
+	Creates a dockerfile using your model spec assuming that sourceCodeFolder contains all the necessray code. 
+	Set the http Flag to create a dockerfile that runs the model as a server instead of a job.
+	`,
 
+	Run: func(cmd *cobra.Command, args []string) {
+		modelSpecificationPath := args[0]
+		sourceCodeFolder := args[1]
+		err := dockerfile.GenerateDockerfile(modelSpecificationPath, sourceCodeFolder, useHttpRunner)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	},
+}
+
+func init() {
+	dockerCmd.Flags().BoolVar(&useHttpRunner, "http", false, "Set flag to package model as an http server instead of a job")
+	rootCmd.AddCommand(dockerCmd)
+	rootCmd.AddCommand(createCmd)
 }
