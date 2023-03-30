63227659a64654b488448889d3894add381590d8
Akash Palrecha <akash@pixxel.co.in>
2023-03-30 18:26:43 +0530

    fix: General bug fixes across ramen (#65) (#66)
    * chore: fixed makefile container build command
    
    * docs: added user-friendly instructions to go-cli
    
    * fix: model spec fixed and updated
    
    * fix: updated task_id parsing
    
    * test: updated tests

--- api/bootstrap/templates/full/src/test_model.py
+++ api/bootstrap/templates/full/src/test_model.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
         {"config": "../specifications/model_specification.yaml"},
     )
     model_input = [
-        {"task_id": "123456"}, # leave this as-is
+        {"name": "task_id", "type": "str", "format": "string", "value": "123456"}, # leave this as-is
         {"name": "input1", "type": "str", "format": "string", "value": "Please modify the "},
-        {"name": "input3", "type": "str", "format": "string", "value": "Types, formats and values"}
+        {"name": "input3", "type": "str", "format": "string", "value": "Types, formats and values"},
         {"name": "input3", "type": "str", "format": "string", "value": "of your inputs appriately"},
     ]
     request = json.dumps(model_input)
--- python/tests/models/ymxplusc.py
+++ python/tests/models/ymxplusc.py
@@ -37,6 +37,8 @@ def make_ymxplusc_input(x: float = 5.0, info: str = "Hopefully this works.") -> 
             "value": info,
         },
     ]
-    request: List[Dict[Any, Any]] = [{"task_id": "please-work"}]
+    request: List[Dict[Any, Any]] = [
+        {"name": "task_id", "type": "str", "format": "string", "value": "123456"}
+    ]
     request.extend(inputs)
     return json.dumps(request)
