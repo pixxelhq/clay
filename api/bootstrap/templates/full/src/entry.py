dd5366c701f4cb0316ba6bcfc7f898a420721a44
Kartik Verma <vkartik97@gmail.com>
2023-07-14 15:29:49 +0530

    feat: abstract type of runner based of env var (#120)
    * feat: abstract type of runner based of env var
    
    * feat: use job runner as default instead of http server

--- api/bootstrap/templates/full/src/entry.py
+++ api/bootstrap/templates/full/src/entry.py
@@ -1,19 +1,26 @@
 import os
 import sys
 
-from clay.runners import JobRunner
 from model import {{.ModelName}}
+
+from clay.runners import JobRunner, HTTPRunner
 
 if __name__ == "__main__":
     args = sys.argv[1]
 
     env = os.getenv("DEXTER_ENV", "dev")
+    runner_type = os.getenv("RUNNER_TYPE", "job")
     specification_path = f"specifications/model_specification_{env}.yaml"
     print(f"Using configuration located at: {specification_path}")
 
-    server = JobRunner(
-        "{{.ModelName}}",
-        {{.ModelName}},
-        {"config": specification_path},
+    runner_class = HTTPRunner if runner_type == "http" else JobRunner
+
+    # TODO(@krtkvrm): args for constructor should be setters/getters
+    #  and its config should be passed when starting the runner
+    runner = runner_class(
+        model_name="{{.ModelName}}",
+        modelcls={{.ModelName}},
+        model_args={"config": specification_path},
     )
-    server.start([args])
+
+    runner.start([args])
