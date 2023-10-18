da502c373572eec066f5560f380c56ce63736df4
Pranjal  Datta <pranjaldatta99@gmail.com>
2023-10-18 15:17:12 +0530

    feat: added new input collection and output submission logic

--- python/clay/exceptions.py
+++ python/clay/exceptions.py
@@ -24,3 +24,14 @@ class FailedExecutionException(Exception):
         super().__init__(message)
         self.msg = message
         self.http_status_code = http_status_code
+
+
+class OutputOverwriteException(Exception):
+    """To be raised when a model is trying to overwrite an output
+
+    Args:
+        Exception (_type_): _description_
+    """
+
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg)
---
+++ python/clay/types.py
@@ -0,0 +1,46 @@
+from typing import List, Union
+
+import pydantic
+from pydantic import Field
+from typing_extensions import Annotated
+
+
+class RasterProperties(pydantic.BaseModel):
+    Bands: Annotated[List[str], Field(serialization_alias="bands")]
+    Source: Annotated[str, Field(serialization_alias="source")]
+    Collection: Annotated[str, Field(serialization_alias="collection")]
+    Dtype: Annotated[str, Field(serialization_alias="dtype")]
+
+
+class VectorProperties(pydantic.BaseModel):
+    Geometry: Annotated[str, Field(serialization_alias="geometry")]
+
+
+class DateProperties(pydantic.BaseModel):
+    FromAoi: Annotated[bool, Field(serialization_alias="from_aoi")]
+
+
+class TabularFileSchema(pydantic.BaseModel):
+    Headers: Annotated[List[str], Field(serialization_alias="headers")]
+
+
+class TabularProperties(pydantic.BaseModel):
+    FileType: Annotated[str, Field(serialization_alias="file_type")]
+    FileSchema: Annotated[TabularFileSchema, Field(serialization_alias="file_schema")]
+
+
+class DataMeta(pydantic.BaseModel):
+    Format: Annotated[str, Field(serialization_alias="format")]
+    Type: Annotated[str, Field(serialization_alias="type")]
+    Name: Annotated[str, Field(serialization_alias="name")]
+    Value: Annotated[
+        Union[int, float, str, str, bool], Field(serialization_alias="value")
+    ]
+
+
+FormatPropertyMap = {
+    "raster": RasterProperties,
+    "vector": VectorProperties,
+    "date": DateProperties,
+    "tabular": TabularProperties,
+}
