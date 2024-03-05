f02eef8b0c2be039fbb3b8293462ba27c301b76c
Kartik Verma <vkartik97@gmail.com>
2024-03-05 21:54:18 +0530

    fix: accept vector data as string even if type is url (#157)
    * fix: accept vector data as string even if type is url
    
    * feat: use json load / dump to write json to file
    
    * feat: extract vector and url type outside
    
    * fix: fixed enum comparison
    
    * fix: pyright
    
    ---------
    
    Co-authored-by: Pranjal  Datta <pranjaldatta99@gmail.com>

--- python/tests/runners/test_job_runner.py
+++ python/tests/runners/test_job_runner.py
@@ -8,6 +8,7 @@ from typing import Any
 
 import pytest
 import shortuuid
+
 from clay import types
 from clay.core import ModelWrapper
 from clay.logger import Logger
@@ -550,4 +551,118 @@ class TestJobRunner(unittest.IsolatedAsyncioTestCase):
 
         assert passed_vals["raster"]["value"] == raster["value"]
         assert passed_vals["string"]["value"] == "hello world"
+        env_patcher.stop()
+
+    async def test_handle_input_assets(self) -> None:
+        class M(ModelWrapper):
+            def __init__(self, config: str, protocol: str = "abfs", logger: Logger = None) -> None:
+                super().__init__(config, protocol, logger)
+
+            def setup(self):
+                pass
+
+            async def preprocess(self, vector_string: types.Vector, vector_file: types.Vector) -> Any:
+                print(vector_file, vector_string)
+                return {"vector_file": vector_file, "vector_string": vector_string}
+
+            async def inference(self, vector_file, vector_string):
+                return {"vector_file": vector_file, "vector_string": vector_string}
+
+            async def postprocess(self, vector_file, vector_string) -> Any:
+                s = types.String(name="string", value="hello world")
+                return {
+                    "string": s,
+                }
+
+        # creating a vector dummy input
+        vector_file = {
+            "format": "vector",
+            "type": "url",
+            "name": "vector_file",
+            "value": "s3://d-platform-orchestrator-lulc-artifacts-s3-01/shapefile.geojson",
+        }
+
+        # creating a dummy string input
+        vector_string = {
+            "format": "vector",
+            "type": "url",
+            "name": "vector_string",
+            "value": '{"type":"Feature","geometry":{"type":"Point","coordinates":[125.6,10.1]},"properties":{"name":"Dinagat Islands"}}',
+        }
+
+        task_id = {
+            "format": "string",
+            "name": "task-id",
+            "type": "str",
+            "value": "task123",
+        }
+        job_id = {
+            "format": "string",
+            "name": "job-id",
+            "type": "str",
+            "value": "job123",
+        }
+        workflow_id = {
+            "format": "string",
+            "name": "workflow-id",
+            "type": "str",
+            "value": "wfk123",
+        }
+        local_working_dir = {
+            "format": "string",
+            "name": "local-working-dir",
+            "type": "str",
+            "value": self.testing_working_dir,
+        }
+        mock_env_vars = copy.deepcopy(self.mock_env_vars)
+        mock_env_vars["AUTO_DOWNLOAD_ASSETS"] = "true"
+        mock_env_vars["REMOTE_PREFIX"] = "s3://d-platform-orchestrator-lulc-artifacts-s3-01"
+        mock_env_vars["DISABLE_AUTO_UPLOAD"] = "false"
+        env_patcher = unittest.mock.patch.dict(os.environ, mock_env_vars)
+        env_patcher.start()
+        a = JobRunner(
+            "dummy",
+            M,
+            {"config": "./tests/runners/dummy-vector-spec.yml"},
+            "./tests/runners/dummy-vector-spec.yml",
+            None,
+        )
+        a.start(args=[vector_file, vector_string, task_id, job_id, workflow_id, local_working_dir])
+        passed_vals = a.get_passed_inputs_dict()
+
+        result_output_dir = os.path.join(
+            self.testing_working_dir,
+            workflow_id["value"],
+            job_id["value"],
+            task_id["value"],
+            "outputs",
+            "result",
+        )
+        remote_raster_path = os.path.join(
+            "s3://d-platform-orchestrator-lulc-artifacts-s3-01",
+            workflow_id["value"],
+            job_id["value"],
+            task_id["value"],
+            "outputs",
+            "result",
+            "clipped.tiff",
+        )
+
+        assert os.path.samefile(
+            passed_vals["raster"]["value"],
+            os.path.join(
+                self.testing_working_dir,
+                workflow_id["value"],
+                job_id["value"],
+                task_id["value"],
+                "inputs",
+                "raster",
+                "test-raster.tiff",
+            ),
+        )
+        assert passed_vals["string"]["value"] == "hello world"
+        assert os.path.exists(os.path.join(result_output_dir, "clipped.tiff"))
+        with open(os.path.join(result_output_dir, "spec.json"), "r") as f:
+            d = json.load(f)
+        assert d["value"] == remote_raster_path
         env_patcher.stop()
