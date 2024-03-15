dcf3cbd3884375ffe30fa52324b93d533405bca1
sanafirdaus <111871996+sanafirdaus@users.noreply.github.com>
2024-03-15 12:25:14 +0530

    feat: added ci for building & testing model (#164)
    * feat: added ci for building & testing model
    
    * fix: set modeltest as dockertag
    
    * fix: removed docker run
    
    * refactor: file name for github action
    
    ---------
    
    Co-authored-by: riteeksrivastav <srivastavriteek@gmail.com>

--- api/bootstrap/utils.go
+++ api/bootstrap/utils.go
@@ -122,6 +122,7 @@ func getTemplateData(modelName string) map[string]TemplateData {
 		"test_main.py":                  TestModel{ModelName: modelName},
 		"pyproject.toml":                PyProject{ModelName: modelName},
 		"package-deploy-model-aws.yaml": GithubWorkflow{ModelName: modelName, Version: Version},
+		"build.yaml":                    GithubWorkflow{ModelName: modelName},
 	}
 	return data
 }
