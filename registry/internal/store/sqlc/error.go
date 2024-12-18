77f6e25b106cd988f749ef8710605d52a611f9a8
riteeksrivastav <srivastavriteek@gmail.com>
2024-12-18 13:32:28 +0530

    fix: return 404 in case block is not found

--- registry/internal/store/sqlc/error.go
+++ registry/internal/store/sqlc/error.go
@@ -1,12 +1,22 @@
 package store
 
 import (
+	"database/sql"
+
 	rerr "github.com/example/clay/registry/pkg/error"
 
 	"github.com/lib/pq"
 )
 
 func PGErrorToRegistryError(err error) *rerr.RegistryError {
+	//This is added because database/sql does not return pq.Error type in case of no rows
+	if err == sql.ErrNoRows {
+		return &rerr.RegistryError{
+			Code: rerr.ErrDoesNotExists,
+			Err:  err,
+		}
+	}
+
 	switch v := err.(type) {
 	case *pq.Error:
 		switch v.Code {
