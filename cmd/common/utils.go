cc7518276d6a69f1f29ee227bcc5dfda3198aa90
Kartik Verma <vkartik97@gmail.com>
2025-04-21 12:39:00 +0530

    fix: remove env specific flags from clay cli (#255)
    * fix: remove env specific flags from clay cli
    
    * fix: remove GetS3Bucket
    
    * fix: latest ubuntu version for CI

--- cmd/common/utils.go
+++ cmd/common/utils.go
@@ -14,58 +14,6 @@ func IsValidVersion(version string) bool {
 
 }
 
-type Url int
-
-const (
-	dev Url = iota
-	stg
-	prod
-)
-
-var EnvGateways = map[Url]string{
-	dev:  "http://orchestrator.d.platform.example.com/",
-	stg:  "http://orchestrator.platform-sandbox.example.com/",
-	prod: "http://orchestrator.platform-staging.example.com/",
-}
-
-var S3bucket = map[Url]string{
-	dev:  "d-platform-clay-public-catalog-s3-01",
-	stg:  "s-platform-clay-public-catalog-s3-01",
-	prod: "p-platform-clay-public-catalog-s3-01",
-}
-
-func (e Url) String() string {
-	return [...]string{"dev", "stg", "prod"}[e]
-}
-
-func GetUrl(env string) (s string) {
-
-	switch env {
-	case "dev":
-		return EnvGateways[dev]
-	case "stg":
-		return EnvGateways[stg]
-	case "prod":
-		return EnvGateways[prod]
-	default:
-		return ""
-	}
-}
-
-func GetS3Bucket(env string) (s string) {
-
-	switch env {
-	case "dev":
-		return S3bucket[dev]
-	case "stg":
-		return S3bucket[stg]
-	case "prod":
-		return S3bucket[prod]
-	default:
-		return ""
-	}
-}
-
 func Getlogger() *logger.Logger {
 
 	logger := logger.NewLogger(&logger.LogConfig{
