dc6baa76ecb4c082df0a35c073b203396b08ffd1
riteeksrivastav <srivastavriteek@gmail.com>
2025-04-21 14:59:36 +0530

    fix: break string version to []int while ordering the block by versions

--- registry/internal/store/sqlc/block_version.sql.go
+++ registry/internal/store/sqlc/block_version.sql.go
@@ -197,7 +197,7 @@ SELECT distinct on (b.id)
 FROM public.block_versions bv 
     INNER JOIN public.blocks b
     ON bv.block_id = b.id
-ORDER BY b.id, bv.version desc
+ORDER BY b.id, string_to_array(regexp_replace(bv.version, '^v', ''), '.')::int[] desc
 `
 
 type GetBlocksWithLatestVersionRow struct {
@@ -263,7 +263,7 @@ FROM public.block_versions bv
     INNER JOIN public.blocks b
     ON bv.block_id = b.id
 WHERE b.name = $1
-ORDER BY bv.version DESC
+ORDER BY string_to_array(regexp_replace(bv.version, '^v', ''), '.')::int[] DESC
 LIMIT 1
 `
 
