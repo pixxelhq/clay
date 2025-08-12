55a7c00de318f0a99b6c7a9d6dae32e38be22179
sanafirdaus <sana@pixxel.co.in>
2025-08-12 15:32:47 +0530

    fix: adds clay failure signal

---
+++ python/clay/_signals.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+from clay import types
+from clay.exceptions import FailedExecutionException
+
+def failure(
+    message: Any = None,
+    http_status_code: int = 500,
+) -> None:
+    """calling clay.failure() returns execution from
+    model to runner context. This behaviour might
+    change in the future.
+    """
+    if message is None:
+        message = "Failure."
+    raise FailedExecutionException(
+        message=message,
+        http_status_code=http_status_code,
+    )
