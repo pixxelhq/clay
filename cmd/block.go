42ffb69f30a7a9aa8c31ca28999344aca5c01fc0
sanafirdaus <sana@pixxel.co.in>
2026-07-09 11:50:33 +0530

    refactor: remove unnecessary environment variable check from BlockCmd

--- cmd/block.go
+++ cmd/block.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
@@ -14,14 +11,6 @@ func BlockCmd() *cobra.Command {
 		Use:   "block <command>",
 		Short: "Manage blocks in the Clay registry",
 		Long:  "List, describe, and manage assets for blocks in the Clay registry.",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			if clayRegistryHost == "" {
-				if clayRegistryHost = os.Getenv("CLAY_REGISTRY_HOST"); clayRegistryHost == "" {
-					return fmt.Errorf("--clay-registry is required (or set CLAY_REGISTRY_HOST env var)")
-				}
-			}
-			return nil
-		},
 	}
 
 	cmd.AddCommand(ListBlockRegistryCmd())
